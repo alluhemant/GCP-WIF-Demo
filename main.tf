6e431d2656e49544fba7b1933cef81d7786c87ba
alluhemant <61874165+alluhemant@users.noreply.github.com>
2025-06-25 17:21:21 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,15 @@
-resource "google_storage_bucket" "my_bucket" {
-  name  = "bck-wif-demo-000"
-  location = "us-central1"
-  project = "My First Project"
+resource "google_storage_bucket" "my-bucket" {
+  name          = "bkt-demo-000"
+  location      = "us-central1"
+  project = "tt-dev-001"
   force_destroy = true
-  public_access_prevention = "enforced"  
+  public_access_prevention = "enforced"
+}
+
+resource "google_storage_bucket" "my-bucket2" {
+  name          = "bkt-demo-002"
+  location      = "us-central1"
+  project = "tt-dev-001"
+  force_destroy = true
+  public_access_prevention = "enforced"
 }
